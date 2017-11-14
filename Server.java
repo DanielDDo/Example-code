c4be0e3f9b5c6b01e0a456b19bdc269f74b79023
dudo <dudo@zone17-eb.cs.ucalgary.ca>
2017-11-14 13:52:33 -0700

    Added examples of client and server code

---
+++ Server.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	public static void main(String[] args) {
+		try {
+			ServerSocket ss = new ServerSocket(1999);
+			Socket s = ss.accept();
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			System.out.println(dis.readUTF());
+		} catch(IOException e) {
+		}
+	}
+}
